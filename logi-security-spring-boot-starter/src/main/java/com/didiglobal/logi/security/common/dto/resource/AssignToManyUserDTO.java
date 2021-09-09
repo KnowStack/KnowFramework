71a817ac41deb4e2cd8649c3c365ca0396a88136
caijiamin_i <caijiamin_i@didiglobal.com>
2021-09-09 14:42:48 +0800

    修复bug

--- logi-security-spring-boot-starter/src/main/java/com/didiglobal/logi/security/common/dto/resource/AssignToManyUserDTO.java
+++ logi-security-spring-boot-starter/src/main/java/com/didiglobal/logi/security/common/dto/resource/AssignToManyUserDTO.java
@@ -21,6 +21,15 @@ import java.util.List;
 @ApiModel(description = "资源权限分配信息，分配用户（某项目，某项目下某资源类别，某项目下某资源类别下某具体资源权限->分配N个用户）")
 public class AssignToManyUserDTO {
 
+    @ApiModelProperty(value = "项目id", dataType = "Integer", required = true)
+    private Integer projectId;
+
+    @ApiModelProperty(value = "资源类别id（如果为null，则表示该项目下的所有具体资源权限都分配给用户list）", dataType = "Integer", required = false)
+    private Integer resourceTypeId;
+
+    @ApiModelProperty(value = "具体资源id（如果为null，则表示该资源类别下的所有具体资源权限都分配给用户list）", dataType = "Integer", required = false)
+    private Integer resourceId;
+
     /**
      * userIdList数组长度可以为0，但是不可为null，表示移除所有old该资源权限与用户的关联信息
      */
@@ -29,4 +38,12 @@ public class AssignToManyUserDTO {
 
     @ApiModelProperty(value = "排除的用户idList（不删除该用户对资源的权限，用于半选状态的用户）", dataType = "List<Integer>", required = false)
     private List<Integer> excludeUserIdList;
+
+    /**
+     * 资源管理级别：
+     * 1（查看权限）
+     * 2（管理权限）
+     */
+    @ApiModelProperty(value = "资源管理级别：1（查看权限）、2（管理权限）", dataType = "Integer", required = true)
+    private Integer controlLevel;
 }
