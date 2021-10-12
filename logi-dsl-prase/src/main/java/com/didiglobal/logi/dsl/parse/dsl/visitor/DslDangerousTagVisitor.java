eff4f390365cd2468f4c19b6b56a2f643672b222
zhaoqingrong <zhaoqingrong@didiglobal.com>
2021-10-12 19:58:09 +0800

    代码修改

--- logi-dsl-prase/src/main/java/com/didiglobal/logi/dsl/parse/dsl/visitor/DslDangerousTagVisitor.java
+++ logi-dsl-prase/src/main/java/com/didiglobal/logi/dsl/parse/dsl/visitor/DslDangerousTagVisitor.java
@@ -12,8 +12,8 @@ import com.didiglobal.logi.dsl.parse.dsl.ast.common.value.JsonNode;
 import com.didiglobal.logi.dsl.parse.dsl.ast.query.Regexp;
 import com.didiglobal.logi.dsl.parse.dsl.ast.query.Wildcard;
 import com.didiglobal.logi.dsl.parse.dsl.visitor.basic.OutputVisitor;
-import com.didiglobal.logi.log.log.ILog;
-import com.didiglobal.logi.log.log.LogFactory;
+import com.didiglobal.logi.log.ILog;
+import com.didiglobal.logi.log.LogFactory;
 import com.google.common.collect.Sets;
 import org.apache.commons.lang.StringUtils;
 
--- logi-log/src/main/java/com.didiglobal.logi.log/Test.java
+++
@@ -1,7 +0,0 @@
-package com.didiglobal.logi.log;
-
-/**
- * @author cjm
- */
-public class Test {
-}
