ed9209dc53153e5fc083233377ccab58f13334c4
zhaoqingrong <zhaoqingrong@didiglobal.com>
2021-11-09 12:09:01 +0800

    修复logIJob 修改版本号 优化清理work、task、lock功能 1.0.15

--- logi-job-spring-boot-starter/src/main/java/com/didiglobal/logi/job/core/beat/BeatManagerImpl.java
+++ logi-job-spring-boot-starter/src/main/java/com/didiglobal/logi/job/core/beat/BeatManagerImpl.java
@@ -3,6 +3,7 @@ package com.didiglobal.logi.job.core.beat;
 import com.didiglobal.logi.job.LogIJobProperties;
 import com.didiglobal.logi.job.common.domain.LogITask;
 import com.didiglobal.logi.job.common.domain.LogIWorker;
+import com.didiglobal.logi.job.common.po.LogITaskLockPO;
 import com.didiglobal.logi.job.common.po.LogITaskPO;
 import com.didiglobal.logi.job.common.po.LogIWorkerPO;
 import com.didiglobal.logi.job.core.WorkerSingleton;
@@ -20,7 +21,8 @@ import org.springframework.util.CollectionUtils;
 
 import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class BeatManagerImpl implements BeatManager {
@@ -55,7 +57,7 @@ public class BeatManagerImpl implements BeatManager {
     @Override
     public boolean beat() {
         logger.info("class=BeatManagerImpl||method=beat||msg=beat beat!!!");
-        cleanWorker();
+        clean();
 
         WorkerSingleton workerSingleton = WorkerSingleton.getInstance();
         workerSingleton.updateInstanceMetrics();
@@ -67,7 +69,6 @@ public class BeatManagerImpl implements BeatManager {
 
     @Override
     public boolean stop() {
-        // clean worker
         WorkerSingleton workerSingleton = WorkerSingleton.getInstance();
         LogIWorker logIWorker = workerSingleton.getLogIWorker();
         logIWorkerMapper.deleteByCode(logIWorker.getWorkerCode());
@@ -75,42 +76,80 @@ public class BeatManagerImpl implements BeatManager {
         return true;
     }
 
-    private void cleanWorker() {
-        long currentTime = System.currentTimeMillis();
-        List<LogIWorkerPO> logIWorkerPOS = logIWorkerMapper.selectByAppName(logIJobProperties.getAppName());
+    /*********************************************** private method ***********************************************/
+    private void clean(){
+        String appName   = logIJobProperties.getAppName();
 
-        if(CollectionUtils.isEmpty(logIWorkerPOS)){return;}
+        Map<String, LogIWorkerPO> logIWorkerPOMap = cleanWorker(appName);
+        cleanTask(appName, logIWorkerPOMap);
+        cleanLock(appName, logIWorkerPOMap);
+    }
+
+    private void cleanTask(String appName, Map<String, LogIWorkerPO> logIWorkerPOMap){
+        List<LogITaskPO>   logITaskPOS   = logITaskMapper.selectByAppName(appName);
+        if(!CollectionUtils.isEmpty(logITaskPOS)){
+            for(LogITaskPO logITaskPO : logITaskPOS){
+                try {
+                    List<LogITask.TaskWorker> taskWorkers = BeanUtil.convertToList(logITaskPO.getTaskWorkerStr(),
+                            LogITask.TaskWorker.class);
+
+                    if(CollectionUtils.isEmpty(taskWorkers)){continue;}
+
+                    Iterator<LogITask.TaskWorker> iter = taskWorkers.iterator();
+                    while (iter.hasNext()) {
+                        LogITask.TaskWorker taskWorker = iter.next();
+
+                        if(null == logIWorkerPOMap
+                                || null == logIWorkerPOMap.get(taskWorker.getWorkerCode())){
+                            iter.remove();
+                        }
+                    }
+
+                    logITaskPO.setTaskWorkerStr(BeanUtil.convertToJson(taskWorkers));
+                    logITaskMapper.updateTaskWorkStrByCode(logITaskPO);
+                }catch (Exception e){
+                    logger.info("class=BeatManagerImpl||method=cleanTask||msg=clean task worker error!", e);
+                }
+            }
+        }
+    }
+
+    private void cleanLock(String appName, Map<String, LogIWorkerPO> logIWorkerPOMap){
+        List<LogITaskLockPO> logITaskLockPOS = logITaskLockMapper.selectByAppName(appName);
+        if(!CollectionUtils.isEmpty(logITaskLockPOS)){
+            Iterator<LogITaskLockPO> iter = logITaskLockPOS.iterator();
+
+            while (iter.hasNext()) {
+                LogITaskLockPO logITaskLockPO = iter.next();
+
+                try {
+                    if(null == logIWorkerPOMap
+                            || null == logIWorkerPOMap.get(logITaskLockPO.getWorkerCode())){
+                        iter.remove();
+                        logITaskLockMapper.deleteByWorkerCodeAndAppName(logITaskLockPO.getWorkerCode(), appName);
+                    }
+                }catch (Exception e){
+                    logger.info("class=BeatManagerImpl||method=cleanLock||msg=clean task worker error!", e);
+                }
+            }
+        }
+    }
+
+    private Map<String, LogIWorkerPO> cleanWorker(String appName) {
+        long currentTime = System.currentTimeMillis();
+
+        List<LogIWorkerPO> logIWorkerPOS = logIWorkerMapper.selectByAppName(logIJobProperties.getAppName());
+        if(CollectionUtils.isEmpty(logIWorkerPOS)){return null;}
 
         for (LogIWorkerPO logIWorkerPO : logIWorkerPOS) {
             if (logIWorkerPO.getHeartbeat().getTime() + 3 * SimpleBeatMonitor.INTERVAL * 1000 < currentTime) {
                 logIWorkerMapper.deleteByCode(logIWorkerPO.getWorkerCode());
-                logITaskLockMapper.deleteByWorkerCodeAndAppName(logIWorkerPO.getWorkerCode(), logIJobProperties.getAppName());
-
-                try {
-                    List<LogITaskPO> logITaskPOS = logITaskMapper.selectByAppName(logIJobProperties.getAppName());
-                    if(CollectionUtils.isEmpty(logITaskPOS)){continue;}
-
-                    for(LogITaskPO logITaskPO : logITaskPOS){
-                        List<LogITask.TaskWorker> taskWorkers = BeanUtil.convertToList(logITaskPO.getTaskWorkerStr(),
-                                LogITask.TaskWorker.class);
-
-                        if(CollectionUtils.isEmpty(taskWorkers)){continue;}
-
-                        Iterator<LogITask.TaskWorker> iter = taskWorkers.iterator();
-                        while (iter.hasNext()) {
-                            LogITask.TaskWorker taskWorker = iter.next();
-
-                            if (Objects.equals(taskWorker.getWorkerCode(), logIWorkerPO.getWorkerCode())) {
-                                iter.remove();
-                            }
-                        }
-
-                        logITaskMapper.updateTaskWorkStrByCode(logITaskPO);
-                    }
-                }catch (Exception e){
-                    logger.info("class=BeatManagerImpl||method=cleanWorker||msg=clean task worker error!", e);
-                }
             }
         }
+
+        List<LogIWorkerPO> logIWorkerPOS1 = logIWorkerMapper.selectByAppName(appName);
+        if(CollectionUtils.isEmpty(logIWorkerPOS1)){return null;}
+
+        return logIWorkerPOS1.stream().collect(Collectors.toMap(LogIWorkerPO::getWorkerCode, (p) -> p));
     }
 }
