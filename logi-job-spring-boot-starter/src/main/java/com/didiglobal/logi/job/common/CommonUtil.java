4a59ae8ab96c3bb9324cb99b3f554fffb15feda7
zhaoqingrong <zhaoqingrong@didiglobal.com>
2022-10-26 13:11:53 +0800

    logi-job 1.0.29 logIJob "/logi-job/logs/list" 接口处理%模糊查询的问题

--- logi-job-spring-boot-starter/src/main/java/com/didiglobal/logi/job/common/CommonUtil.java
+++ logi-job-spring-boot-starter/src/main/java/com/didiglobal/logi/job/common/CommonUtil.java
@@ -1,15 +1,17 @@
 package com.didiglobal.logi.job.common;
 
+import org.springframework.util.StringUtils;
+
 public class CommonUtil {
 
     /**
      * 避免模糊查询把查询条件中的"% _"当作通配符处理（造成结果是全量查询）
      */
     public static String sqlFuzzyQueryTransfer(String str){
-        if(str.contains("%")){
+        if(!StringUtils.isEmpty(str) && str.contains("%")){
             str = str.replaceAll("%", "\\\\%");
         }
-        if(str.contains("_")){
+        if(!StringUtils.isEmpty(str) && str.contains("_")){
             str = str.replaceAll("_","\\\\_");
         }
         return str;
