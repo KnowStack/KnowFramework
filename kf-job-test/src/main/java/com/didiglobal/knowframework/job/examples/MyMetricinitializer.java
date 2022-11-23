64bfacefec47f56e5c4c33f4a6ec9e00375ef8fb
huqidong <williamhuqidong@didiglobal.com>
2022-11-24 02:07:57 +0800

    1.）kf-observability 支持指标实时获取接口.

--- kf-job-test/src/main/java/com/didiglobal/knowframework/job/examples/MyMetricinitializer.java
+++ kf-job-test/src/main/java/com/didiglobal/knowframework/job/examples/MyMetricinitializer.java
@@ -1,22 +1,28 @@
 package com.didiglobal.knowframework.job.examples;
 
 import com.didiglobal.knowframework.observability.conponent.metrics.BaseMetricInitializer;
+import com.didiglobal.knowframework.observability.conponent.metrics.Meter;
+import com.didiglobal.knowframework.observability.conponent.metrics.Metric;
 import com.didiglobal.knowframework.observability.conponent.metrics.MetricUnit;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MyMetricinitializer extends BaseMetricInitializer {
+public class MyMetricInitializer extends BaseMetricInitializer {
 
     @Override
     public void register() {
-        Map<String, String> tags = new HashMap<>();
-        tags.put("docType", "humanities");
         super.registerMetric(
                 "docs",
                 "number of docs",
                 MetricUnit.METRIC_UNIT_NUMBER,
-                1.0,
-                tags
+                new Meter() {
+                    @Override
+                    public Metric getMetric() {
+                        Map<String, String> tags = new HashMap<>();
+                        tags.put("docType", "humanities");
+                        return new Metric(1.0, tags);
+                    }
+                }
         );
     }
 
